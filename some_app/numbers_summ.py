fae72718a8df4a040d0fd31dc9caf463c8976f53
ogerasymenko <alexandr.x2@gmail.com>
2017-12-08 22:04:00 +0200

    Refactoring numbers.py

--- some_app/numbers_summ.py
+++ some_app/numbers_summ.py
@@ -1,3 +1,5 @@
+import argparse
+
 def func_info(func):
     """Decorator for printing info about passed function"""
     def func_name(*args):
@@ -10,19 +12,38 @@ def func_info(func):
 
 
 @func_info
-def num_func(num_list):
+def num_func(arg):
     """Function accept list with integers, print unique pairs wich equal 10
     and returns None"""
+    num_list = []
     arr = []
-    for i in num_list:
-        for n in num_list[1:]:
-            if (i+n == 10) and ((arr.count((i, n)) < 1) and (arr.count((n, i)) < 1)):
-                arr.append((i, n))
+    
+    if type(arg) == list:
+        num_list = arg
+    elif type(arg) == tuple:
+        num_list = list(arg)
+    elif type(arg) == str:
+        num_list = [int(x.strip()) for x in arg.split(',')]
+    else:
+        raise TypeError('Wrong input data type')
+    
+    for index, element in enumerate(num_list):
+        for next_element in num_list[index+1:]:
+            if (element+next_element == 10) and \
+            ((arr.count((element, next_element)) < 1) and 
+             (arr.count((next_element, element)) < 1)):
+                arr.append((element, next_element))
             else:
-                continue 
+                continue
+    print num_list
+    print arr
     return arr
 
 if __name__ == '__main__':
-    result = num_func((1,2,3,4,5,5,6))
-    for r in result:
-        print r[0], '+', r[1]
+    parser = argparse.ArgumentParser(description='Numbers via comma for processing.')
+    parser.add_argument('-n', '--numbers', type=str, required=True, help='Numbres like 1,2,3')
+    args = parser.parse_args()
+    result = num_func(args.numbers)
+    if result:
+        for r in result:
+            print r[0], '+', r[1]
